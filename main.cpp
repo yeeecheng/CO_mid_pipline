562ccf5c43f8083ccae0e6dc60dd10d90dd15b15
yeeecheng <yichengliao1003@gmail.com>
2022-12-27 19:15:46 +0800

    add cmd input file path

--- main.cpp
+++ main.cpp
@@ -5,11 +5,29 @@
 #include "CPU_pipeline.h"
 using namespace std;
 
-int main(){
-    ifstream ifs("./test_data/q1.txt",ios::in);
+int main(int argc,char **argv){
+
+    // 預設檔案路徑
+    string file_path ="./test_data/q1.txt";
+    
+    // 判斷是否放超過一個的檔案路徑
+    if(argc > 2){
+        
+        cout<<"only input one file path\n";
+        // 使用 -1 代表程式執行失敗
+        return -1;
+    }
+    // 使用額外輸入的檔案路徑
+    else if(argc>1){
+        file_path = argv[1];
+    }
+    
+    ifstream ifs(file_path,ios::in);
     if(!ifs.is_open()){
+
         cout<<"fail to open this file"<<endl;
-        return 1;
+        // 使用 -1 代表程式執行失敗
+        return -1;
     }
 
     stringstream ss;
